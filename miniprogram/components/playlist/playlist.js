782cb134531a4973181029afb76fccf913a3cf6e
Alixxxe <2177897664@qq.com>
2021-01-26 22:48:44 +0800

    Initial Commit

---
+++ miniprogram/components/playlist/playlist.js
@@ -0,0 +1,34 @@
+// components/playlist/playlist.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    playlist: {
+      type: Object,
+    }
+  },
+  observers:{
+    ['playlist.playCount'](count){
+      this.setData({
+        _count: this._tranNumber(count,2)
+      })
+    }
+  },
+ 
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+   
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ miniprogram/pages/music/music.js
@@ -0,0 +1,127 @@
+// pages/music/music.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls:[{
+        url:'http://p1.music.126.net/nVUH7O5ZNMG1OQ1kE-tq9g==/109951165665595417.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/SLfispSeeEnb6Ezs0cNjBw==/109951165666128356.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
+      },
+      {
+          url:'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'
+      },
+      {
+          url:'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+      },
+      {
+          url:'http://p1.music.126.net/UdSM2BmqY_h_t9HAOzb5dQ==/109951165664710664.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/Z90NF2dHuBYrV6x-U9jJJQ==/109951165664719544.jpg?imageView&quality=89'
+      },
+      {
+      url:'http://p1.music.126.net/vAjwukVm-H0LOqzy4FTJXA==/109951165664851877.jpg?imageView&quality=89'
+      },
+      {
+       url:'http://p1.music.126.net/j0gp3gBDRRoqIXxAs0v7oA==/109951165664720877.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/9Ayx-EeCnuLRWKTcIhGB6g==/109951165664742856.jpg?imageView&quality=89'
+      }],
+  
+ 
+  playlist:[{
+    "id":"1001",
+    "playCount": 1.4641238e+86,
+    "name":"Pitchfork评选70年代最佳200首歌曲",
+    "picUrl":"http://p1.music.126.net/SLfispSeeEnb6Ezs0cNjBw==/109951165666128356.jpg?imageView&blur=40x20"
+    },
+    {  "id" :"1002",
+    "playCount": 1.5641238e+86,
+    "name":"你一定要在自己热爱的世界里闪闪发亮啊",
+    "picUrl":"http://p3.music.126.net/uesfHcJmZ23S3er_1mpeaw==/109951165621856219.jpg?param=200y200"
+    },
+    {  "id":"1003",
+    "playCount": 1.6641238e+86,
+    "name":"我试着把孤独藏进耳机",
+    "picUrl":"http://p4.music.126.net/Xvo6PwBcdOA69ipcpV9YYg==/109951165463253777.jpg?param=200y200"
+    },
+    {  "id":"1004",
+    "playCount": 1.7641238e+86,
+    "name":"【翻/原】温柔不是我说 而是你觉得",
+    "picUrl":"http://p3.music.126.net/PJylNWy_2-jI7LRgQ2Cm6w==/109951165649129522.jpg?param=200y200"
+    },
+    {  "id":"1005",
+    "playCount": 1.8641238e+86,
+    "name":"2021·心里装着鲜花银河星光和我爱的人",
+    "picUrl":"http://p4.music.126.net/O8LkkfC7PtV7TA4UP693XA==/109951164569667332.jpg?param=200y200"
+    },
+    {  "id":"1006",
+    "playCount": 1.9641238e+86,
+    "name":"认真生活 度过一个充实的寒假叭",
+    "picUrl":"http://p4.music.126.net/zhF9g1skZ_XqrBXC8Z7s4g==/109951165645315295.jpg?param=200y200"
+    }]
+ },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
